870d1fd9844c092c335391c0af57abfeb88f09bd
Joshua Cecka <josh@cecka.org>
2023-06-01 21:12:59 -0700

    Fixed up and down.

--- Main.py
+++ Main.py
@@ -2,21 +2,20 @@ import math
 from textwrap import wrap
 
 
-words = ['abc', 'lkji', 'dhl', 'iem']        
-raw = ("abcdefghijklmnop")
+words = ['abc', 'lkji', 'dhl', 'mie', 'bfj']        
+raw = "abcdefghijklmnop"
 size = int(math.sqrt(len(raw)))
-found = []
 
 
 def checkRight(word):
-    table = wrap(raw, size)
+    table = wrap(raw, size) #split string into list
     for x in table:
         if x.find(word) != -1:
             return(str(x.find(word)) + ',' + str(table.index(x)))
 
 
 def checkLeft(word):
-    word = word[:: -1]
+    word = word[:: -1] #reverse word
     table = wrap(raw, size)
     for x in table:
         if x.find(word) != -1:
@@ -26,22 +25,31 @@ def checkLeft(word):
 def checkDown(word):
     table = wrap(raw, size)
     table_down = []
-    for i in range(len(table[0])):
+    for x in range(len(table[0])):
         downraw = ''
-        for j in range(len(table)):
-            downraw = downraw + (table[j][i])
+        for y in range(len(table)):
+            downraw = downraw + (table[y][x])
+        table_down.append(downraw)
+    for x in table_down:
+        if x.find(word) != -1:
+            return(str(table_down.index(x)) + ',' + str(x.find(word)))
+
+def checkUp(word):
+    word = word[:: -1]
+    table = wrap(raw, size)
+    table_down = []
+    for x in range(len(table[0])):
+        downraw = ''
+        for y in range(len(table)):
+            downraw = downraw + (table[y][x])
         table_down.append(downraw)
     for x in table_down:
         if x.find(word) != -1:
             return(str(table_down.index(x)) + ',' + str(x.find(word)+len(word)-1))
-checkDown('BEAR')
-
-def checkUp(word):
-    
-    return
 
 
 def findWords():
+    found = []
     for x in words:
         if checkRight(x) != None:
             found.append(x + ': ' + str(checkRight(x)) + ' (Right)')
@@ -51,6 +59,9 @@ def findWords():
 
         if checkDown(x) != None:
             found.append(x + ': ' + str(checkDown(x)) + ' (Down)')
+
+        if checkUp(x) != None:
+            found.append(x + ': ' + str(checkUp(x)) + ' (Up)')
 
     return(found)
 
@@ -64,15 +75,3 @@ print(findWords())
 # 1   e f g h
 # 2   i j k l 
 # 3   m n o p
-
-
-# # # # # # #
-#     0 1 2 3
-#  
-# 0   a e i m
-# 1   b f j n
-# 2   c g k o 
-# 3   d h l p
-
-#1) swap x and y (x,y = y,x)
-#2) 
